d348d2bd8b92414a865651d6d223ad4ccecb9761
Danila Ferents <iambpev@gmail.com>
2019-04-07 14:34:25 +0300

    Add wagonadmin

---
+++ wagonadmin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  wagonadmin
+//
+//  Created by Мой Господин on 07/04/2019.
+//  Copyright © 2019 Danila Ferents. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
