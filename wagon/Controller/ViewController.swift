5eb6bce4f2ea59fead71b750a7788f75e8fbdbc3
Danila Ferents <iambpev@gmail.com>
2019-04-14 19:35:47 +0300

    Add Register VC

--- wagon/Controller/ViewController.swift
+++ wagon/Controller/ViewController.swift
@@ -12,9 +12,12 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
+    override func viewDidAppear(_ animated: Bool) {
+        let storyboard = UIStoryboard(name: "LoginStoryboard", bundle: nil)
+        let controller = storyboard.instantiateViewController(withIdentifier: "loginVC")
+        present(controller, animated: true, completion: nil)
+    }
 
 }
 
